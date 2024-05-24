d2cfc2afc739c94c6ccd7db1f75d12d39cc32876
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-05-24 13:30:32 +0100

    Improve dev environment
    Add support for multiple deployments in dev environment
    Remove workspaceFolder from devcontainer config
    Update README to use odin-data devcontainer
    Create odin extra for local development
    Print prompt to delete existing local deployment

--- dev/configure.sh
+++ dev/configure.sh
@@ -2,27 +2,36 @@
 
 # Script to populate deployment with absolute paths of environment
 
-if [[ "$1" == "-h" || "$1" == "--help" || "$#" -ne 2 ]]; then
-    echo "Usage: $0 <path-to-odin-data-prefix> <path-to-venv>"
+if [[ "$1" == "-h" || "$1" == "--help" || "$#" -ne 3 ]]; then
+    echo "Usage: $0 <deployment-name> <path-to-odin-data-prefix> <path-to-venv>"
     exit 0
 fi
 
-ODIN_DATA=$1
-VENV=$2
+DEPLOYMENT=$1
+ODIN_DATA=$2
+VENV=$3
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-mkdir ${SCRIPT_DIR}/local
-cp ${SCRIPT_DIR}/templates/* ${SCRIPT_DIR}/local
+LOCAL=${SCRIPT_DIR}/local
 
-SERVER="${SCRIPT_DIR}/local/stOdinServer.sh"
-FR="${SCRIPT_DIR}/local/stFrameReceiver1.sh"
-FR_CONFIG="${SCRIPT_DIR}/local/fr1.json"
-FP="${SCRIPT_DIR}/local/stFrameProcessor1.sh"
-FP_CONFIG="${SCRIPT_DIR}/local/fp1.json"
-META="${SCRIPT_DIR}/local/stMetaWriter.sh"
-LAYOUT="${SCRIPT_DIR}/local/layout.kdl"
+# Check if local already exists
+if [ -d ${LOCAL} ]; then
+    echo "Local deployment ${LOCAL} already exists. Please remove it if you want to replace it."
+    exit 1
+fi
+
+mkdir ${LOCAL}
+cp ${SCRIPT_DIR}/${DEPLOYMENT}/* ${LOCAL}
+
+SERVER="${LOCAL}/stOdinServer.sh"
+FR="${LOCAL}/stFrameReceiver*.sh"
+FR_CONFIG="${LOCAL}/fr*.json"
+FP="${LOCAL}/stFrameProcessor*.sh"
+FP_CONFIG="${LOCAL}/fp*.json"
+META="${LOCAL}/stMetaWriter.sh"
+LAYOUT="${LOCAL}/layout.kdl"
 
 sed -i "s+<ODIN_DATA>+${ODIN_DATA}+g" ${FR} ${FR_CONFIG} ${FP} ${FP_CONFIG}
 sed -i "s+<VENV>+${VENV}+g" ${SERVER} ${META}
-sed -i "s+<SCRIPT_DIR>+${SCRIPT_DIR}/local+g" ${LAYOUT}
+sed -i "s+<SCRIPT_DIR>+${LOCAL}+g" ${LAYOUT}
---
+++ dev/two_node_fp/stMetaWriter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+<VENV>/bin/meta_writer --data-endpoints tcp://127.0.0.1:10008
