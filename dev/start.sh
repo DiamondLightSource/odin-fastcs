2526b07951421c69183bf9916595ca8d487a858c
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-06-19 15:04:08 +0000

    Fix variable name

--- dev/start.sh
+++ dev/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-ZELLIG_CONFIG="-l ${SCRIPT_DIR}/local/layout.kdl"
+ZELLIJ_CONFIG="-l ${SCRIPT_DIR}/local/layout.kdl"
 
-zellij ${ZELLIG_CONFIG} || bash <(curl -L zellij.dev/launch) ${ZELLIG_CONFIG}
+zellij ${ZELLIJ_CONFIG} || bash <(curl -L zellij.dev/launch) ${ZELLIJ_CONFIG}
