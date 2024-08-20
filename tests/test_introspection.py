7467e1e4efc6751f59e92484ad187030166b7c81
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-08-20 14:36:39 +0100

    Add some additional targetted testing on param creation

--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -41,3 +41,29 @@ async def test_fp_initialise(mocker: MockerFixture):
     controller = FrameProcessorAdapterController(mock_connection, parameters, "prefix")
     await controller.initialise()
     assert all(fpx in controller.get_sub_controllers() for fpx in ("FP0", "FP1"))
+
+
+def test_node_with_empty_list_is_correctly_counted():
+    parameters = create_odin_parameters({"test": []})
+    names = [p.name for p in parameters]
+    assert "test" in names
+    assert len(parameters) == 1
+
+
+def test_node_that_has_metadata_only_counts_once():
+    data = {"count": {"value": 1, "writeable": False, "type": "int"}}
+    parameters = create_odin_parameters(data)
+    assert len(parameters) == 1
+
+
+def test_nested_node_gives_correct_name():
+    data = {"top": {"nest-1": {"nest-2": 1}}}
+    parameters = create_odin_parameters(data)
+    assert len(parameters) == 1
+    assert parameters[0].name == "top_nest-1_nest-2"
+
+
+def test_config_node_splits_list_into_mutiples():
+    data = {"config": {"param": [1, 2]}}
+    parameters = create_odin_parameters(data)
+    assert len(parameters) == 2
