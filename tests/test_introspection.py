304440e6d7d4fde09472377a1e614f0633cbd221
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-08-20 08:37:53 +0000

    Correctly handle parameters with empty lists as values

--- tests/test_introspection.py
+++ tests/test_introspection.py
@@ -15,7 +15,7 @@ def test_one_node():
         response = json.loads(f.read())
 
     parameters = create_odin_parameters(response)
-    assert len(parameters) == 96
+    assert len(parameters) == 97
 
 
 def test_two_node():
@@ -23,7 +23,7 @@ def test_two_node():
         response = json.loads(f.read())
 
     parameters = create_odin_parameters(response)
-    assert len(parameters) == 188
+    assert len(parameters) == 190
 
 
 @pytest.mark.asyncio
